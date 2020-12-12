1632a629d3cc3a81c4dd10d1799d632d85c2ac2b
voyevoda <elisa9el@gmail.com>
2020-12-12 14:38:13 -0500

    More webpack config, minify CSS in prod and include Terser loader to make sure js is minified as well

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html',
-    }),
-  ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,11 @@
 const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
 const path = require('path');
+const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,5 +13,34 @@ module.exports = merge(common, {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'public'),
   },
-  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/template.html',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+        ],
+      },
+    ],
+  },
 });
