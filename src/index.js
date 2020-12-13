92d820b36d3da93da34e3f50e517bc8bdb36f823
voyevoda <elisa9el@gmail.com>
2020-12-12 22:34:21 -0500

    Using mock data to create components since I ran into difficulties with Axios

--- src/index.js
+++ src/index.js
@@ -4,20 +4,85 @@ import './style.css';
 // Classes
 import PetAPI from './utils/PetAPI';
 
+import axios from 'axios';
+import { ALL_PETS } from './utils/setup';
+
 /*
  * TO DO:
  * Get user's location and then make the API call
  * Make a second call for cats and then combine the two into one object/array
+ * Make a new API call for the next 50 when pager is clicked/lazy loading?
  */
 
+/*
 const petApi = new PetAPI();
-const params = {
-  params: {
-    geo_range: 50,
-    city_or_zip: 47374,
-    species: 'dog',
-    output: 'json',
+const params = [
+  {
+    params: {
+      geo_range: 50,
+      city_or_zip: 47374,
+      species: 'dog',
+      output: 'json',
+    },
   },
+  {
+    params: {
+      geo_range: 50,
+      city_or_zip: 47374,
+      species: 'cat',
+      output: 'json',
+    },
+  },
+];
+
+const dogs = petApi.getPets(params);
+console.log(dogs);
+params.params.species = 'cat';
+const cats = petApi.getPets(params);
+
+const pets = dogs.concat(cats);
+
+console.log(pets);
+
+
+const pets = petApi.getAllPets(params);
+
+console.log(pets);
+console.log('Do I wait?');
+*/
+
+const params = [
+  {
+    params: {
+      geo_range: 50,
+      city_or_zip: 47374,
+      species: 'dog',
+      output: 'json',
+    },
+  },
+  {
+    params: {
+      geo_range: 50,
+      city_or_zip: 47374,
+      species: 'cat',
+      output: 'json',
+    },
+  },
+];
+
+/*
+const getAllPets = (query) => {
+  const url = `${BASE_API_URL}pet_search?key=${API_KEY}`;
+  Promise.all([axios.get(url, query[0]), axios.get(url, query[1])])
+    .then((res) => {
+      const dogs = res[0].data.pets;
+      const cats = res[1].data.pets;
+      const allPets = dogs.concat(cats);
+      return allPets;
+    })
+    .catch((err) => console.log(err));
 };
 
-petApi.getPets(params);
+const pets = (params) => getAllPets(params);
+console.log(pets(params));
+*/
--- src/utils/PetAPI.js
+++ src/utils/PetAPI.js
@@ -1,4 +1,10 @@
-import { API_KEY, AXIOS, BASE_API_URL } from './setup';
+import axios from 'axios';
+import { API_KEY, BASE_API_URL } from './setup';
+
+/*
+ * TO DO:
+ * Handle errors 404 etc
+ */
 
 class PetAPI {
   constructor() {
@@ -6,24 +12,32 @@ class PetAPI {
     this._baseUrl = BASE_API_URL;
   }
 
-  async getPets(query) {
+  getPets(query) {
     const url = `pet_search?key=${this._apiKey}`;
-    try {
-      const response = await AXIOS.get(url, query);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+    axios
+      .get(url, query)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   }
 
-  async getPet(query) {
+  getAllPets(query) {
+    const url = `${this._baseUrl}pet_search?key=${this._apiKey}`;
+    Promise.all([axios.get(url, query[0]), axios.get(url, query[1])])
+      .then((res) => {
+        const dogs = res[0].data.pets;
+        const cats = res[1].data.pets;
+        const allPets = dogs.concat(cats);
+        return allPets;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  getPet(query) {
     const url = `pet_details?key=${this._apiKey}`;
-    try {
-      const response = await AXIOS.get(url, query);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
+    axios
+      .get(url, query)
+      .then((res) => res)
+      .catch((err) => err);
   }
 }
 
