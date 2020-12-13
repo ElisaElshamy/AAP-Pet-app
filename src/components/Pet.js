14ded36e2bcd0d02a10b612bcfd90aa01aec21e0
voyevoda <elisa9el@gmail.com>
2020-12-13 00:24:31 -0500

    Created class Pet with method createCard

---
+++ src/components/Pet.js
@@ -0,0 +1,41 @@
+class Pet {
+  constructor(petObj) {
+    this.url = petObj.details_url;
+
+    /* We will get all properties later in a method called getPetInfo() */
+    this.name = petObj.pet_name;
+    this.breed = petObj.secondary_breed
+      ? `${petObj.primary_breed}/${petObj.secondary_breed}`
+      : petObj.primary_breed;
+    this.gender = petObj.sex === 'f' ? 'Female' : 'Male';
+    this.age = petObj.age ? petObj.age : 'Unknown';
+    this.location = `${addr_city}, ${addr_state_code}`;
+  }
+
+  createCard() {
+    const card = document.createElement('div');
+    card.classList.add('card', 'bg-white', 'shadow-md');
+
+    const petDetails = document.createElement('div');
+    petDetails.classList.add('pet-details', 'flex', 'flex-col');
+
+    const petName = document.createElement('h4');
+    petName.innerHTML = this.name;
+
+    const breed = document.createElement('h5');
+    breed.innerHTML = this.breed;
+
+    const info = document.createElement('span');
+    info.innerHTML = `${this.gender}, ${this.age}`;
+
+    const location = document.createElement('span');
+    location.innerHTML = this.location;
+
+    petDetails.append(petName, breed, info, location);
+    card.appendChild(petDetails);
+
+    return card;
+  }
+}
+
+export default Pet;
